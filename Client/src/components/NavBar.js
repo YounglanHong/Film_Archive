b1793feb48b589b4bee33e2a64b47257af4aa570
YounglanHong <yong2401@gmail.com>
2020-08-07 10:20:26 +0900

    Feat: Add responsive navbar

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-// import Home from "../pages/Home";
-// import Movie from "../components/Movie/Movie";
-// import Search from "../components/Search/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import "../styles/navbar.css";
 
-export default function NavBar(props) {
+export default function NavBar() {
+  let [navbar, setNavbar] = useState("navbar_items");
+  function navbarClick() {
+    if (navbar === "navbar_items") {
+      setNavbar("navbar_items_menu");
+    } else if (navbar === "navbar_items_menu") {
+      setNavbar("navbar_items");
+    }
+  }
   return (
     <div className="NavBar">
-      <div className="navbar_items">
+      <i className="navbar_menu">
+        <ExpandMoreIcon onClick={navbarClick} />
+      </i>
+      <div className={navbar}>
+        {console.log(navbar)}
         <Link to="/" className="navbar_item">
           Home
         </Link>
