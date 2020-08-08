8bcfb0d562111fefe8292d25e2fc06394aab6ea1
YounglanHong <yong2401@gmail.com>
2020-08-08 11:30:08 +0900

    Modify responsive navbar

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -7,20 +7,24 @@ import "../styles/navbar.css";
 
 export default function NavBar() {
   let [navbar, setNavbar] = useState("navbar_items");
+
   function navbarClick() {
     if (navbar === "navbar_items") {
-      setNavbar("navbar_items_menu");
-    } else if (navbar === "navbar_items_menu") {
+      setNavbar("navbar_items_toggle");
+    } else if (navbar === "navbar_items_toggle") {
       setNavbar("navbar_items");
     }
   }
+
+  const className = window.innerWidth > 768 ? "navbar_items" : navbar;
+
   return (
     <div className="NavBar">
-      <i className="navbar_menu">
+      <i className="navbar_expand">
         <ExpandMoreIcon onClick={navbarClick} />
       </i>
-      <div className={navbar}>
-        {console.log(navbar)}
+      <div className={className}>
+        {console.log("className", className)}
         <Link to="/" className="navbar_item">
           Home
         </Link>
