7c961c27d9fdef9bdedb0e4bfb056df3fc9e7af0
YounglanHong <yong2401@gmail.com>
2020-08-09 20:19:19 +0900

    Feat: Set up middleware and nodemon, Create register function
      - Install body-parser and cookie-parser
      - Install nodemon
      - Send post request to register

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
 mongoose
-  .connect(
-    "mongodb+srv://YounglanHong:dudfks92@filmarchive.d8lxb.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log(err));
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("error", err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/register", (req, res) => {
+  // Put Client Login data to MongoDB
+  const user = new User(req.body);
+  // MongoDB Save & Error Handling
+  user.save((err, userData) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({ sucess: true });
+  });
 });
 
 app.listen(PORT, () => {
