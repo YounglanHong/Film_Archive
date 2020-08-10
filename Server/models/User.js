49a7b11d19e77f2cfef5779eb7c0eba4b4087cf0
YounglanHong <yong2401@gmail.com>
2020-08-10 22:18:30 +0900

    Feat: Hash password with bycrypt and Add login function using jwt

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +16,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 4,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -22,6 +26,7 @@ const userSchema = mongoose.Schema({
     type: Number,
     default: 0, // normal user
   },
+  image: String,
   token: {
     type: String,
   },
@@ -29,6 +34,70 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// Encryption
+userSchema.pre("save", function (next) {
+  let user = this;
+
+  // Condition: Encrypt password only when password is modified
+  if (user.isModified("password")) {
+    // Encrypt password(https://www.npmjs.com/package/bcrypt)
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (plainPassword, callback) {
+  // Encrypt plain password
+  // Compare with hashed password
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    // if (err) return callback(err), callback(null, isMatch);
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.createToken = function (callback) {
+  let user = this;
+  // Create token using jwt
+  let token = jwt.sign(user._id.toHexString(), "filmArchive");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, callback) {
+  let user = this;
+
+  // Decode token
+
+  jwt.verify(token, "filmArchive", function (err, decoded) {
+    // Find user by userId
+    // Compare client token with DB stored token
+
+    user.findOne(
+      {
+        _id: decoded,
+        token: token,
+      },
+      function (err, user) {
+        if (err) return callback(err);
+        callback(null, user);
+      }
+    );
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
