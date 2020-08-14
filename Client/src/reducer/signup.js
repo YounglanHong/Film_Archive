993d3ee92fcd77bff6ff15aff5b91fa0788dc537
YounglanHong <yong2401@gmail.com>
2020-08-14 18:31:45 +0900

    Add Signup page & action, reducer

---
+++ Client/src/reducer/signup.js
@@ -0,0 +1,16 @@
+import { SIGN_UP } from "../action/signupAction";
+
+let initialState = {
+  name: "test",
+  email: "test@gmail.com",
+  password: "*****",
+};
+
+export default function signUp(state = initialState, action) {
+  switch (action.type) {
+    case SIGN_UP:
+      return { ...state, register: action.payload };
+    default:
+      return state;
+  }
+}
