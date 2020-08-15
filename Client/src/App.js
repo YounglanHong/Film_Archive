f091d11b9f9190826ddcc3682436ba91c0be41b2
YounglanHong <yong2401@gmail.com>
2020-08-15 16:30:27 +0900

    Feat: Apply HOC Auth to App component

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SearchRoot from "./pages/SearchRoot";
+import MovieRoot from "./pages/MovieRoot";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
+
+import Auth from "./hoc/auth";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Home} />
+        <Header />
+        <Route exact path="/" component={Auth(Home, null)} />
+        <Route exact path="/search" component={Auth(SearchRoot, null)} />
+        <Route
+          exact
+          path="/movie/:id/:title"
+          component={Auth(MovieRoot, true)}
+        />
+        <Route exact path="/signin" component={Auth(SignIn, false)} />
+        <Route exact path="/signout" component={Auth(SignOut, true)} />
+        <Route exact path="/signup" component={Auth(SignUp, null)} />
+        {/* <Route exact path="reviews/:id/:title" component={Reviews} /> */}
+        {/* <Footer /> */}
       </div>
     );
   }
