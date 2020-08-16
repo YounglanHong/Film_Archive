b239b9f8947bab1b5f7c08ca307adbfb59fd1886
YounglanHong <yong2401@gmail.com>
2020-08-16 17:39:26 +0900

    Refactor: Create index file and combine reducers
      - Add signin, signup, auth reducers to index

--- Client/src/reducer/index.js
+++ Client/src/reducer/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import signin from "./signin";
+import signup from "./signup";
+import auth from "./auth";
 
 // 분리된 리듀서 결합(combineReducers)
 const rootReducer = combineReducers({
   signin,
+  signup,
+  auth,
 });
 
 export default rootReducer;
