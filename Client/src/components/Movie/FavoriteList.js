eb5742731b279252db6d7885836129d385656d1b
YounglanHong <yong2401@gmail.com>
2020-08-19 23:47:23 +0900

    Feat: Create FavoriteList and WatchList

---
+++ Client/src/components/Movie/FavoriteList.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Search from "../Search/Search";
+// import
+// import { API_KEY, API_URL, IMAGE_URL } from "../config";
+
+import "../../styles/movie.css";
+
+export default function FavoriteList() {
+  return (
+    <div className="FavoriteList">
+      {/* movie */}
+      {/* <Search /> */}
+    </div>
+  );
+}
---
+++ Client/src/components/Movie/WatchList.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Search from "../Search/Search";
+// import
+// import { API_KEY, API_URL, IMAGE_URL } from "../config";
+
+import "../../styles/movie.css";
+
+export default function WatchList(props) {
+  return (
+    <div className="WatchList">
+      {/* movie */}
+      {/* <Search /> */}
+    </div>
+  );
+}
