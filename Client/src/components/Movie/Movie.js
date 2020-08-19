216f448b827e2e05d08b22b742e9e306876abed5
YounglanHong <yong2401@gmail.com>
2020-08-19 23:50:13 +0900

    Feat: Add favorite & watched actions to Movie

--- Client/src/components/Movie/Movie.js
+++ Client/src/components/Movie/Movie.js
@@ -3,12 +3,22 @@ import axios from "axios";
 import MovieDetail from "./MovieDetail";
 import { API_KEY, API_URL, IMAGE_URL } from "../../config";
 
+import { useDispatch } from "react-redux";
+
+import { addFav } from "../../action/movieAction";
+import { addWatched } from "../../action/movieAction";
+
 import "../../styles/movie.css";
 
 export default function Movie(props) {
-  // console.log(props.id);
+  const dispatch = useDispatch();
+  // console.log(props);
+  const { userId, email, name } = props;
+
   let [results, setResults] = useState("");
   let [keywords, setKeywords] = useState([]);
+  let [favorite, setFavorite] = useState(false);
+  let [watched, setWatched] = useState(false);
 
   useEffect(() => {
     // 영화 id로 영화 정보 요청
@@ -36,8 +46,54 @@ export default function Movie(props) {
       });
   }, []);
 
+  //* favorite 관리
+  function handleFavorite(e) {
+    // 새로고침 방지
+    e.preventDefault();
+
+    let body = {
+      email: email,
+      name: name,
+      userId: userId,
+    };
+
+    dispatch(addFav(body)).then((res) => {
+      console.log(res);
+      if (res.payload.userId) {
+        alert("Add to Favorite");
+        setFavorite(true);
+      } else {
+        setFavorite(false);
+      }
+    });
+  }
+
+  //* watched 관리
+  function handleWatched(e) {
+    // 새로고침 방지
+    e.preventDefault();
+
+    let body = {
+      email: email,
+      name: name,
+      userId: userId,
+    };
+
+    dispatch(addWatched(body)).then((res) => {
+      console.log(res);
+      if (res.payload.userId) {
+        alert("Add to Watched");
+        setWatched(true);
+      } else {
+        setWatched(false);
+      }
+    });
+  }
+
   return (
     <div className="Movie">
+      <button onClick={handleFavorite}>Favorite</button>
+      <button onClick={handleWatched}>Watched</button>
       <MovieDetail results={results} keywords={keywords} />
     </div>
   );
--- Client/src/pages/MovieRoot.js
+++ Client/src/pages/MovieRoot.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Movie from "../components/Movie/Movie";
+import { connect } from "react-redux";
 
-export default function MovieRoot(props) {
-  console.log(props.location.state);
-
+function MovieRoot(props) {
+  // console.log(props);
   const { id } = props.location.state;
+  const { userId, email, name } = props;
+  // const { userData } = props;
   return (
     <div className="MovieRoot">
-      <Movie id={id} />
+      <Movie id={id} userId={userId} email={email} name={name} />
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return {
+    userId: state.user.userId,
+    email: state.user.email,
+    name: state.user.name,
+  };
+};
+
+export default connect(mapStateToProps)(MovieRoot);
