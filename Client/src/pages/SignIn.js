53bcfcc59a7400296d0ab79cc27b8d41a93ea90f
YounglanHong <yong2401@gmail.com>
2020-08-31 10:26:25 +0900

    Style: Add style to sign in

--- Client/src/pages/SignIn.js
+++ Client/src/pages/SignIn.js
@@ -1,16 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { signIn } from "../action/signinAction";
+
+import { TEST_EMAIL } from "../config";
+import { TEST_PASSWORD } from "../config";
 
 import { Link } from "react-router-dom";
 
 import "../styles/signin.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+//style
+const useStyles = makeStyles({
+  input_email: {
+    margin: "50px 0px 10px 0px",
+  },
+  input_password: {
+    margin: "10px 0px 30px 0px",
+  },
+  button: { margin: "10px 0px 50px 0px" },
+});
+// theme
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#f4ff81",
+      main: "#eeff41",
+      dark: "#aeea00",
+      contrastText: "#c6ff00",
+    },
+  },
+});
 
 export default function SignIn(props) {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    setEmail(TEST_EMAIL);
+    setPassword(TEST_PASSWORD);
+  });
 
   // console.log(props);
 
@@ -57,31 +95,54 @@ export default function SignIn(props) {
   return (
     <div className="SignIn">
       <div className="signin_container">
-        <form
-          className="signin_form"
-          onSubmit={handleSubmit}
-          onKeyPress={enterKeySignIn}
-        >
-          <h3>SignIn</h3>
-          <label>Email</label>
-          <input type="email" value={email} onChange={handleEmail} />
-          <label>Password</label>
-          <input type="password" value={password} onChange={handlePassword} />
+        <ThemeProvider theme={theme}>
+          <Box className="signin_box">
+            <Typography component="h1" variant="h5" style={{ color: "black" }}>
+              Sign in
+            </Typography>
 
-          <br />
+            <form
+              className="signin_form"
+              onSubmit={handleSubmit}
+              onKeyPress={enterKeySignIn}
+            >
+              <p>testmail로 로그인 하세요:)</p>
 
-          <button type="submit">SignIn</button>
+              <TextField
+                label="Email"
+                variant="filled"
+                value={email}
+                onChange={handleEmail}
+                className={classes.input_email}
+              />
+              <TextField
+                label="Password"
+                variant="filled"
+                value={password}
+                type="password"
+                onChange={handlePassword}
+                className={classes.input_password}
+              />
+              <Button
+                onClick={handleSubmit}
+                variant="contained"
+                className={classes.button}
+              >
+                Sign In
+              </Button>
+            </form>
+          </Box>
 
-          <hr />
-          <div>
+          {/* <div>
             <p>New to Film Archive?</p>
-            <button>
+            <Button disabled variant="contained">
               <Link to="/signup" className="link">
                 Create an Acount
               </Link>
-            </button>
-          </div>
-        </form>
+            </Button>
+          </div> */}
+          {/* </form> */}
+        </ThemeProvider>
       </div>
     </div>
   );
