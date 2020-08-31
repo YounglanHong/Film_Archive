1d84db8692945ab59978eeda0441e740c557d4da
YounglanHong <yong2401@gmail.com>
2020-08-31 09:08:59 +0900

    Style: Change Post Review text field style and color

--- Client/src/components/Movie/Review/PostReview.js
+++ Client/src/components/Movie/Review/PostReview.js
@@ -3,10 +3,38 @@ import { useDispatch } from "react-redux";
 
 import { addReview } from "../../../action/movieAction";
 
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+//style
+const useStyles = makeStyles({
+  input_title: {
+    margin: "50px 30px 20px 30px",
+  },
+  input_text: {
+    margin: "0px 30px 10px 30px",
+  },
+  button: { margin: "10px 30px 0px 30px" },
+});
+// theme
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#f4ff81",
+      main: "#eeff41",
+      dark: "#aeea00",
+      contrastText: "#c6ff00",
+    },
+  },
+});
+
 export default function PostReview(props) {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const { userId, movieId, image } = props.location.state;
-  // console.log(userId);
 
   let [title, setTitle] = useState("");
   let [review, setReview] = useState("");
@@ -42,14 +70,38 @@ export default function PostReview(props) {
   }
 
   return (
-    <div className="Reveiw">
-      <div>Review</div>
-      <p>글자수 제한: 100자(띄어쓰기 포함)</p>
-      <label>Title</label>
-      <input type="text" value={title} onChange={handleTitle} />
-      <label>Review</label>
-      <input type="text" value={review} onChange={handleReview} />
-      <button onClick={postReview}>Post Review</button>
+    <div className="PostReview">
+      <h1>Review</h1>
+      {/* <p>글자수 제한: 100자(띄어쓰기 포함)</p> */}
+      <ThemeProvider theme={theme}>
+        <div className="postreview_container">
+          <TextField
+            label="Title"
+            variant="outlined"
+            value={title}
+            onChange={handleTitle}
+            className={classes.input_title}
+          />
+          <TextField
+            label="Review"
+            variant="filled"
+            multiline
+            autoFocus
+            rows="25"
+            value={review}
+            onChange={handleReview}
+            className={classes.input_text}
+          />
+          <div className="postreview_limit">{review.length}/100</div>
+          <Button
+            onClick={postReview}
+            variant="contained"
+            className={classes.button}
+          >
+            Post Review
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
