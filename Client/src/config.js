aaf64220f79e9efe44d2d426cc59637ae078eaa1
YounglanHong <yong2401@gmail.com>
2020-08-31 11:06:27 +0900

    heroku deployment

---
+++ Client/src/config.js
@@ -0,0 +1,7 @@
+// movie api
+export const API_KEY = "0b838f37d63f1772b58e394e366504ce";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+export const TEST_EMAIL = "test@testmail.com";
+export const TEST_PASSWORD = "dudfks";
